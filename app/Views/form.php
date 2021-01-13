33dda08afe0c7895001d9107ea48d7de04779b77
David042 <Davidsarka242@gmail.com>
2021-01-12 23:17:03 -0800

    upload of Cloned form validation

---
+++ app/Views/form.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<h1>User Login</h1>
+	<form class="form_control" action="success" method="GET">
+		<div class="label">*Email or Phone</div>
+		<i class="fa fa-user 3x"></i>
+		<input type="text" name="name" placeholder="Username">
+		<div class="error_message">
+			Invalid Email or Phone input!
+		</div>
+		<div class="label">*Password</div>
+		<i class="fa fa-user fa-lock 3x"></i>
+		<input type="password" name="password" placeholder="Password">
+		<div class="error_password">
+			Invalid Password Input!
+		</div>
+		<input type="submit" value="Login">
+	</form>
+</div>
---
+++ app/Views/valid_page.php
@@ -0,0 +1,12 @@
+<h2>Login Successful!</h2>
+
+<?php
+	echo $name = $_GET["name"];
+	echo $password = $_GET["password"];
+	if(!empty($name)){
+
+	}
+	else{
+		echo "Username should not be Empty!";
+	}
+?>
